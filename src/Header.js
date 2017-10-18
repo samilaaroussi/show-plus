0d8563042d341c6c8c5c7bad7687ba8cf57a50ba
Sami Laaroussi Tribek <sami@slt.fr>
2017-10-19 00:59:52 +0200

    add logo

--- src/Header.js
+++ src/Header.js
@@ -1,9 +1,7 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import axios from 'axios';
 import Affix from "react-overlays/lib/Affix";
 import {Image, Navbar, Collapse, Modal, Button, Row, Col, Grid} from 'react-bootstrap';
-import _ from 'lodash';
 import './App.css';
 
 class Header extends Component {
@@ -20,7 +18,7 @@ class Header extends Component {
         <Navbar className="navbar-default navbar-fixed-top">
           <Navbar.Header>
             <Navbar.Brand>
-              <Image src="/img/logo.png" className="App-logo" alt="logo"/>
+              <Image src="/img/logo2.png" className="App-logo" alt="logo"/>
             </Navbar.Brand>
           </Navbar.Header>
         </Navbar>
